8abdad02ff5c021a0b84ecb2005bd4b1bb68f9ac
ouyuanning <52741323@qq.com>
2022-06-17 14:03:23 +0800

    add decimal constant in plan2

--- pkg/sql/parsers/dialect/mysql/mysql_sql.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql.go
@@ -12238,10 +12238,10 @@ yydefault:
 			}
 			if ival > 0 {
 				yyLOCAL = tree.NewNumValWithResInt(constant.MakeInt64(ival), yylex.(*Lexer).scanner.LastToken, false, ival)
-				yyLOCAL.ValType = tree.P_int64
+				yyLOCAL.ValType = tree.P_uint64
 			} else {
 				yyLOCAL = tree.NewNumValWithResInt(constant.MakeInt64(ival), yylex.(*Lexer).scanner.LastToken, true, ival)
-				yyLOCAL.ValType = tree.P_int64
+				yyLOCAL.ValType = tree.P_uint64
 			}
 		}
 		yyVAL.union = yyLOCAL
@@ -12259,7 +12259,7 @@ yydefault:
 //line mysql_sql.y:5322
 		{
 			ival := util.GetUint64(yyDollar[1].item)
-			yyLOCAL = tree.NewNumValWithType(constant.MakeUint64(ival), yylex.(*Lexer).scanner.LastToken, false, tree.P_int64)
+			yyLOCAL = tree.NewNumValWithType(constant.MakeUint64(ival), yylex.(*Lexer).scanner.LastToken, false, tree.P_uint64)
 		}
 		yyVAL.union = yyLOCAL
 	case 953:
@@ -12309,7 +12309,7 @@ yydefault:
 		var yyLOCAL tree.Expr
 //line mysql_sql.y:5349
 		{
-			yyLOCAL = tree.NewNumValWithType(constant.MakeString(yyDollar[1].str), yyDollar[1].str, false, tree.P_decimal128)
+			yyLOCAL = tree.NewNumValWithType(constant.MakeString(yyDollar[1].str), yyDollar[1].str, false, tree.P_decimal)
 		}
 		yyVAL.union = yyLOCAL
 	case 959:
